10bdf543ebf6fbdcd54a0c05223f522619f5bcde
Alexander Bartosik <alex.bartosik@gmail.com>
2024-02-08 12:25:41 +0100

    A2 incomplete

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -5,15 +5,30 @@
 #include <algorithm>
 #include "word.h"
 #include "dictionary.h"
+#include <unordered_set>
 
 using std::string;
 using std::vector;
+#include <fstream>
+#include <iostream>
+
+std::unordered_set<std::string> words;
 
 Dictionary::Dictionary() {
+	std::ifstream inputFile("words_in.txt");
+	std::string word;
+
+	while (inputFile >> word) {
+        words.insert(word);
+    }
+	inputFile.close(); //behövs detta?
 }
 
 bool Dictionary::contains(const string& word) const {
-	return true;
+	if (words.find(word) != words.end()) {
+		return true;
+	} 
+	return false;
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
