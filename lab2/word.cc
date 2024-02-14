4d1b8faa574238506d275781814d0e66a590ce8a
Alexander Bartosik <alex.bartosik@gmail.com>
2024-02-14 10:53:55 +0100

    word.cc first implement

--- lab2/word.cc
+++ lab2/word.cc
@@ -5,12 +5,36 @@
 using std::vector;
 using std::string;
 
-Word::Word(const string& w, const vector<string>& t) {}
+class Word {
+private:
+	string word;
+	vector<string> trigrams;
 
+public: 
+
+//Creates a word w with the sorted trigrams t
+Word::Word(const string& w, const vector<string>& t) : word(w), trigrams(t) {} 
+
+//return word
 string Word::get_word() const {
-	return string();
+	return word;
 }
 
+//Returns how many of the trigrams in t that are present in this word’s trigram vector
 unsigned int Word::get_matches(const vector<string>& t) const {
-	return 0;
+	int count = 0;
+	//sort t
+	std::sort(t.begin(), t.end());
+	std::sort(trigrams.begin(), trigrams.end()); //just in case :))
+
+	int maxIndex = std::min(t.size(),trigrams.size());
+
+	for (int i = 0; i < maxIndex; i++) {
+		if (t[i].compare(trigrams[i]) == 0) {
+			count++;
+		}
+	}
+	return count;
 }
+
+};
